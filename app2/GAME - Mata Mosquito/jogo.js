c83fec69cbe326979a42aa02039655caae6cc0a5
Paulo Henrique de Souza Vinhal <ph.vinhal@hotmail.com>
2021-02-13 18:48:27 -0300

    13/02/2021
    Estudos de hoje

---
+++ app2/GAME - Mata Mosquito/jogo.js
@@ -0,0 +1,67 @@
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura,altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica(){
+
+    if(document.getElementById('mosca')){
+        document.getElementById('mosca').remove()
+    }
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+    //criar o elemento html
+
+    var mosca = document.createElement('img')
+    mosca.src = 'imagens/mosca.png'
+    mosca.className = tamanhosRandomicos() + ' ' + ladoRandomico()
+    mosca.style.left = posicaoX + 'px'
+    mosca.style.top = posicaoY + 'px'
+    mosca.style.position = 'absolute'
+    mosca.id = 'mosca'
+
+    document.body.appendChild(mosca)
+    
+
+    
+}
+
+function tamanhosRandomicos(){
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch(classe){
+        case 0:
+            return 'mosca1'
+        case 1:
+            return 'mosca2'
+        case 2:
+            return 'mosca3'
+    }
+
+}
+
+function ladoRandomico(){
+    var classe = Math.floor(Math.random() * 2)
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
+
+
